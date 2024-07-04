7319c7bea620d2b05c3a0a5856292f7010041fc0
kevin <kevinlarioshernandez@gmail.com>
2024-07-04 14:56:23 -0600

    commit

--- funcion.js
+++ funcion.js
@@ -1,25 +1,21 @@
 document.getElementById('promedioForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const nota1Input = document.getElementById('nota1');
-    const nota2Input = document.getElementById('nota2');
-    const nota1 = parseFloat(nota1Input.value);
-    const nota2 = parseFloat(nota2Input.value);
+    const nota1Input = parseFloat(document.getElementById('nota1').value);
+    const nota2Input = parseFloat(document.getElementById('nota2').value);
 
     const inputs = [nota1Input, nota2Input];
     inputs.forEach(input => {
-        if (!input.validity.valid) {
-            input.classList.add('is-invalid');
-        } else {
-            input.classList.remove('is-invalid');
+        if (isNaN(input)) {
+            document.getElementById('resultado').textContent = "Ingrese números válidos";
         }
     });
 
-    if (!nota1Input.validity.valid || !nota2Input.validity.valid) {
+    if (isNaN(nota1Input) || isNaN(nota2Input)) {
         return;
     }
 
-    const promedio = (nota1 + nota2) / 2;
+    const promedio = (nota1Input + nota2Input) / 2;
     document.getElementById('resultado').textContent = promedio.toFixed(2);
 });
 
